91756e33ca47722d03f6353afb8ea5734b338e8d
Martin Šidlo <sejdlov@gmail.com>
2020-05-24 20:32:05 +0200

    api - mocking

---
+++ src/mocks/axios.js
@@ -0,0 +1,19 @@
+import MockAdapter from 'axios-mock-adapter'
+
+const DEFAULT_REQUEST_TIMEOUT = 1000
+
+export function mockAxios(axios) {
+
+  const mock = new MockAdapter(axios)
+  const url = new RegExp(`/*`);
+
+  mock.onGet(url).reply(config => {
+    // All calls for get must have .json file with mocked response
+    const response = require(`./${config.url.replace('/', '')}.json`)
+    return new Promise(function (resolve, reject) {
+      setTimeout( () => {
+        resolve([ 200, response ])
+      }, DEFAULT_REQUEST_TIMEOUT);
+    })
+  })
+}
